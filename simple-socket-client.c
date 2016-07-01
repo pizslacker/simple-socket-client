d76d6a5c522394d352dd60db8cd4d55b71dc2ba3
Kim Olsen <pizslacker@users.noreply.github.com>
2016-07-01 05:19:11 +0200

    Uploaded first-draft simple-socket-client.

---
+++ simple-socket-client.c
@@ -0,0 +1,79 @@
+/******************************************************
+/ Name:          Simple Socket Client (in C)
+/ Created:       20160629-13:01-GMT+1
+/
+/ Creator:       Kim Olsen <post@kimeriktjoraolsen.no>
+/ Lastchanhge:   20160701-05:10-GMT+1
+/
+/ Simple socket client, implemented in C.
+/
+******************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+   int sockfd, portno, n;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   char *vers = "0.1";
+   char buffer[256]; /* 256-char buffer */
+   
+   /* Print helptext if less than 3 arguments supplied to run-command */
+   if (argc < 3) {
+      fprintf(stderr,"Simple Socket Client v%s (64-bit)\n", vers);
+      fprintf(stderr,"Usage: %s <hostname> <port-number>\n", argv[0]);
+      exit(0);
+   }	
+   portno = atoi(argv[2]);
+   
+   /* Create a socket point */
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+   server = gethostbyname(argv[1]);
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+   }
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(portno);
+   
+   /* Now connect to the server */
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      exit(1);
+   }
+   
+   /* Now ask for a message from the user, this message
+    * will be read by socket-server.
+    */	
+   printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+   
+   /* Send message to the server */
+   n = write(sockfd, buffer, strlen(buffer));   
+   if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+   }
+   
+   /* Now read server response */
+   bzero(buffer,256);
+   n = read(sockfd, buffer, 255);   
+   if (n < 0) {
+      /* Error > exit 1 */
+      perror("ERROR reading from socket");
+      exit(1);
+   }	
+   printf("%s\n",buffer);
+   return 0;
+}
